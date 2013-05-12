4a45f7fc1b95fb4465a664d246fba9b88ea7e892
lamasu <rafaerthal@gmail.com>
2013-05-12 13:42:23 -0300

    implementando strcmp

---
+++ trab02/teste.c
@@ -0,0 +1,19 @@
+/*
+ * Descricao: 	exemplo de programa escrito em C que chama uma 
+ *		funcÃ£o definida em linguagem de montagem.
+ * Author:	Divino CÃ©sar
+ * Data:	08 de maio de 2013
+ *
+ */
+#include <stdio.h>
+
+extern int my_strcmp(char *str, char *str);
+
+int main(int argc, char *argv[]) {
+
+	int res = my_strcmp(argv[1], argv[2]);
+
+	printf("my_strcmp(%s, %s) = %d\n", argv[1], argv[2], res);
+
+	return 0;	
+}
--- trab02/trab02.s
+++ trab02/trab02.s
@@ -1,9 +1,10 @@
+@ MC404 - Projeto 2 (parte 1)
+@ Conjunto de funcoes para manipulacÃ£o de vetores de caracteres
+@ e conversÃ£o de bases.
+@
 @ aluno : Rafael Almeida Erthal Hermano
 @ ra    : 121286
 @
-@ MC404 - Projeto
-@ primeira parte do projeto, biblioteca strutils.s
-
 .globl my_ahtoi
 .globl my_atoi
 .globl my_itoa
@@ -11,55 +12,107 @@
 .globl my_strcmp
 .globl my_strlen
 
-.data
-string_end: .byte '\0'
-line_break: .byte '\n'
-
 .text
 .align 4
 
-@ descricao : converte uma string que representa um hexa em um decimal
+@ Converte uma cadeia de caracteres com dígitos hexadecimais terminada em zero
+@ para um inteiro de 32 bits.
+@
 @ entrada   : {r0: endereco da string}
 @ saida     : {r0: valor em decimal}
+@
 my_ahtoi:
-    push {ip, lr}
+    push {lr}
 
-    pop {ip, pc}
+    pop {pc}
 
-@ descricao : converte uma string que representa um decimal em decimal
+@ Converte uma cadeia de caracteres com dígitos decimais terminada em zero para
+@ um inteiro de 32 bits.
+@
 @ entrada   : {r0: endereco da string}
 @ saida     : {r0: valor em decimal}
+@
 my_atoi:
-    push {ip, lr}
+    push {lr}
 
-    pop {ip, pc}
+    pop {pc}
 
-@ descricao : converte um inteiro em string
+@ Converte um inteiro para uma cadeia de caracteres com dígitos decimais
+@ representando o inteiro. A cadeia deve ser preenchina na memória a partir do
+@ endereço fornecido em buf e deve ser terminada com zero.
+@
 @ entrada   : {r0: valor do inteiro, r1: endereco da string de saida}
+@
 my_itoa:
-    push {ip, lr}
+    push {lr}
 
-    pop {ip, pc}
+    pop {pc}
 
-@ descricao : converte um inteiro em uma string que representa um hexadecimal
+@ Converte um inteiro para uma cadeia de caracteres com dígitos hexadecimais
+@ representando o inteiro. A cadeia deve ser preenchina na memória a partir do
+@ endereço fornecido em buf e deve ser terminada com zero.
+@
 @ entrada   : {r0: endereco da string, r1: endereco da string de saida}
+@
 my_itoah:
-    push {ip, lr}
+    push {lr}
 
-    pop {ip, pc}
+    pop {pc}
 
-@ descricao : compara duas strings
+@ Compara as duas cadeias de caracteres apontadas por s1 e s2. Retorna 0 se
+@ ambas forem iguais. -1 Se a primeira for lexicograficamente menor e 1 se a
+@ segunda for lexicograficamente menor.
+@
 @ entrada   : {r0: endereco da primeira string, r1: endereco da segunda string}
 @ saida     : {r0: resultado da compacarao}
+@
 my_strcmp:
-    push {ip, lr}
+    push {lr}
+    push {r3}
+    push {r4}
 
-    pop {ip, pc}
+    mov r3, r1
+    mov r4, r0
 
-@ descricao : retorna o tamanho de uma string
+my_strcmp_head:
+    @ carrego os caracteres da memória
+    ldrb r1, [r3], #0x1
+    ldrb r2, [r4], #0x1
+
+    @ verifico se alguma das strings chegou ao final
+    cmp r1, #0
+    beq my_strcmp_tail 
+    cmp r2, #0
+    beq my_strcmp_tail 
+    
+    @ verifico se as strings continuam sendo iguais
+    cmp r1, r2
+    beq my_strcmp_head
+
+my_strcmp_tail:
+    cmp r1, r2
+    moveq r0, #0
+    movgt r0, #1
+    movlt r0, #-1
+
+    pop {r3}
+    pop {r4}
+    pop {pc}
+
+@ retorna o tamanho de uma string
+@
 @ entrada   : {r0: endereco da string}
 @ saida     : {r0: tamanho da string}
+@
 my_strlen:
-    push {ip, lr}
+    push {lr}
+    mov r2, r0
+my_strlen_head:
+    ldrb r1, [r0], #0x1
+    cmp r1, #0x0
+    bne my_strlen_head
+    
+    sub r0, r0, r2
+    sub r0, r0, #0x1
 
-    pop {ip, pc}
+    pop {pc}
